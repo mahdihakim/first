ed5e001ecef9c103c417bd44f96c4b2515662ab8
mahdihakim <mahdihakim444@gmail.com>
2022-08-04 11:43:55 +0200

    equation

--- Main.java
+++ Main.java
@@ -1,5 +1,40 @@
-public class Main {
+import java.util.Scanner; // import the Scanner class 
+
+
+class Main {
   public static void main(String[] args) {
-    System.out.println("Hello World!");
+    //Define variables
+    Scanner mynumber = new Scanner(System.in);
+    int a,b,c;
+    double delta,x1,x2;
+
+    //Get a,b,c
+    System.out.println("donner un nombre a:"); 
+    a = mynumber.nextInt();   
+       
+    System.out.println("donner un nombre b:"); 
+    b = mynumber.nextInt();  
+
+    System.out.println("donner un nombre c:"); 
+    c = mynumber.nextInt();  
+
+    //Compute delta
+    delta=b*b - 4*a*c;
+
+    //Test delta value
+    if(delta >0)
+    {
+      x1=(-b-Math.sqrt(delta))/(2*a);
+      x2=(-b+Math.sqrt(delta))/(2*a);
+      System.out.println("Deux racines : x1 = " +x1 + " x2 = "+x2);
+    }
+    else if (delta==0)
+    {
+      x1=-b/(2*a);
+      System.out.println("solution double : x = " +x1 );      
+    }
+    else 
+      System.out.println("pas de solution");
+
   }
 }
