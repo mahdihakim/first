d4dc0348917431471acd9ffbe070f5820fc215a8
mahdihakim <mahdihakim444@gmail.com>
2022-08-04 12:06:05 +0200

    ex somme avec while

--- Main.java
+++ Main.java
@@ -1,40 +1,21 @@
 import java.util.Scanner; // import the Scanner class 
 
-
 class Main {
   public static void main(String[] args) {
     //Define variables
     Scanner mynumber = new Scanner(System.in);
-    int a,b,c;
-    double delta,x1,x2;
+    int s=0 , n;
 
-    //Get a,b,c
-    System.out.println("donner un nombre a:"); 
-    a = mynumber.nextInt();   
-       
-    System.out.println("donner un nombre b:"); 
-    b = mynumber.nextInt();  
+    //Get n
+    System.out.println("donner un nombre n:"); 
+    n = mynumber.nextInt();   
 
-    System.out.println("donner un nombre c:"); 
-    c = mynumber.nextInt();  
-
-    //Compute delta
-    delta=b*b - 4*a*c;
-
-    //Test delta value
-    if(delta >0)
-    {
-      x1=(-b-Math.sqrt(delta))/(2*a);
-      x2=(-b+Math.sqrt(delta))/(2*a);
-      System.out.println("Deux racines : x1 = " +x1 + " x2 = "+x2);
+    //Compute sum 
+    int i = 0;
+    while (i <= n) {
+     s=s+i;
+      i++;
     }
-    else if (delta==0)
-    {
-      x1=-b/(2*a);
-      System.out.println("solution double : x = " +x1 );      
-    }
-    else 
-      System.out.println("pas de solution");
-
-  }
+    System.out.println("s="+s);
+}
 }
